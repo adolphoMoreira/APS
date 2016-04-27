eeeb938e8647d11d6e760bf5b077ab8d872e27d7
Adolpho Moreira <Adolpho.Moreira@lhsgroup.com>
2016-04-27 14:05:11 -0300

    Initial commit

---
+++ appRtc/www/js/controllers.js
@@ -0,0 +1,55 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('CamerasCtrl', function($scope) {
+  $scope.cameras = [
+    { title: 'Câmera 1', id: 1 },
+    { title: 'Câmera 2', id: 2 },
+    { title: 'Câmera 3', id: 3 },
+    { title: 'Câmera 4', id: 4 },
+    { title: 'Câmera 5', id: 5 }
+  ];
+})
+
+.controller('CameraCtrl', function($scope, $stateParams) {
+});
